0a82258f4e5d2822650b1d2bb79c3806eb1e0fec
Nishia <pinlacnishia@gmail.com>
2025-04-08 04:34:32 +0800

    Add files via upload

---
+++ iainjobseeker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace iainjobseeker
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
